787923ec27d875b749eadc7ba151fff0dad447ed
TheGreatOneNamedZach <thegreatonenamedzach@gmail.com>
2023-09-10 03:52:33 -0400

    Added auto.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/initialize/auto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/initialize/auto.java
@@ -1,10 +1,119 @@
 package org.firstinspires.ftc.teamcode.initialize;
 
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
-public class auto extends LinearOpMode {
+import org.firstinspires.ftc.teamcode.action.mecanumDrive;
+import org.firstinspires.ftc.teamcode.other.customSensors.imageDetection;
+
+import java.util.Objects;
+
+@Autonomous(name = "Auto", group = "")
+public class auto extends OpMode {
+    // CONSTRUCT
+    public ElapsedTime autoRuntime = new ElapsedTime(); // How long the autonomous has run for
+    ElapsedTime actionRuntime = new ElapsedTime(); // How long the current action has run for
+    org.firstinspires.ftc.teamcode.action.mecanumDrive mecanumDrive = new mecanumDrive();
+    org.firstinspires.ftc.teamcode.other.customSensors.imageDetection imageDetection = new imageDetection();
+    // DECLARE NULL
+    int tempDuck; // Stores the name of any newly found image. This will be null when no NEW image is found
+    // DECLARE CUSTOM
+    int robotAction = 0; // Keeps track of which action the bot is currently doing
+    double length = 1.0; // The time the action runs for
+    double[] distance = {-1, -1, -1, -1};
+    int duck = 0; // Stores the name of the found image that has the highest confidence. This is the same as "tempDuck" but is never null
+    public String teamColor = "Red"; // Which alliance we are currently on
+    Boolean tFInitHasRun = false; // Has the TensorFlow initialise method run already?
+    Boolean teamSelected = false; // Has the primary driver selected an alliance?
+    Boolean startLeftSide = true;
+    Boolean failedColor = false;
+    private static double timeLimit = 0.00;
+
+    // METHODS
+    /** Initializes the autonomous. */
+    public void init(){
+        mecanumDrive.init(this);
+        imageDetection.init(this);
+        mecanumDrive.setMaxSpeed(1.00); // Sets the maximum speed of the wheels. Slow-mode is enabled automatically so this is really 0.5
+        mecanumDrive.runWithoutEncoder();
+    }
+
+    /** Loops until the start button is pressed. */
+    public void init_loop(){
+        tempDuck = imageDetection.imageReturn(); // The variable "tempDuck" contains the latest detected image name (if any)
+        if(tempDuck != 0){ // If "tempDuck" has a detected image name, it is more recent than what is currently in "duck"
+            duck = tempDuck; // Since we know the image name in "duck" is outdated, we update it to whatever image name is in "tempDuck"
+        }
+
+        // TELEMETRY
+        telemetry.addData("Image", duck); // Displays the name of the latest image to the phone screen
+        if(duck == 0) { // If no image has been found, display "Loading..." on the phone screen
+            telemetry.addData("Status", "Loading...");
+        } else { // If an image has been found, tell the drivers that autonomous can now be run
+            telemetry.addData("Status", "Ready! You can now run Autonomous.");
+        }
+        imageDetection.imageReturnTel(); // Outputs data about all detected images to the phone screen
+    }
+
+    /** Runs one time when the autonomous starts. */
+    public void start() {
+        autoRuntime.reset(); // Resets both timers
+        actionRuntime.reset();
+    }
+
+    /** Loops until the stop button is pressed. */
     @Override
-    public void runOpMode() throws InterruptedException {
+    public void loop() {
+        // TELEMETRY
+        telemetry.addData("Time Elapsed For Autonomous", autoRuntime.seconds()); // Time since the autonomous has started
+        telemetry.addData("Time Elapsed For Action", actionRuntime.time()); // Displays the current time since the action has started
+        telemetry.addData("Robot Action", robotAction); // Displays the current action the robot is on
 
+        if(duck <= 1) {
+            if(robotAction == 1) {
+                mecanumDrive(1.0, 1, 0, 0);
+            } else if (robotAction == 2) {
+                mecanumDrive(0.5, 0, 1, 0);
+            }
+        }
+
+        if (duck == 2) {
+            if(robotAction == 1) {
+                mecanumDrive(1.5, 1, 0, 0);
+            }
+        }
+
+        if(duck == 3) {
+            if(robotAction == 1) {
+                mecanumDrive(1.0, 1, 0, 0);
+            } else if (robotAction == 2) {
+                mecanumDrive(0.5, 0, -1, 0);
+            }
+        }
+    }
+
+    public void mecanumDrive(double secondsToRunFor, double yAxisPower, double xAxisPower, double rotationInPower) {
+        yAxisPower = yAxisPower * -1;
+        mecanumDrive.setPower(xAxisPower, yAxisPower, rotationInPower);
+        if(actionRuntime.time() >= secondsToRunFor){ // If this action runs longer than it should...
+            robotAction++; // Go to the next action
+            actionRuntime.reset(); // Reset the timer
+            mecanumDrive.setPower(0, 0, 0);
+        }
+    }
+
+    private void waitThenGoToNextAction(double secondsToWait) {
+        if(actionRuntime.time() >= secondsToWait){ // If this action runs longer than it should...
+            robotAction++; // Go to the next action
+            actionRuntime.reset(); // Reset the timer
+        }
+    }
+
+    /** Stops the robot. */
+    @Override
+    public void stop() {
+        imageDetection.stop();
+        super.stop(); // Stops the OpMode
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/other/customSensors/imageDetection.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/other/customSensors/imageDetection.java
@@ -5,6 +5,7 @@ import androidx.annotation.NonNull;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
+import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
 import org.firstinspires.ftc.vision.VisionPortal;
@@ -22,7 +23,8 @@ public class imageDetection {
     public TfodProcessor tfod;
     public AprilTagProcessor aprilTag;
     public VisionPortal visionPortal;
-    private static String label;
+    private int label;
+    Telemetry telemetry;
     // DECLARE CUSTOM
     private static float confidence = -1;
 
@@ -32,6 +34,7 @@ public class imageDetection {
      */
     public void init(@NonNull OpMode opMode){
         hardwareMap = opMode.hardwareMap;
+        telemetry = opMode.telemetry;
 
         // Create the TensorFlow processor by using a builder.
         tfod = new TfodProcessor.Builder()
@@ -96,7 +99,7 @@ public class imageDetection {
     }
 
     /** Returns the label of a new image, if any. */
-    public String imageReturn() {
+    public int imageReturn() {
         if (tfod != null) {
             // getFreshRecognitions() will return null if no new information is available since
             // the last time that call was made.
@@ -105,15 +108,48 @@ public class imageDetection {
                 for (Recognition recognition : updatedRecognitions) {
                     if (recognition.getConfidence() > confidence && Math.abs(recognition.getRight() - recognition.getLeft()) <= 175) {
                         confidence = recognition.getConfidence();
-                        label = recognition.getLabel();
+                        float yPixel = ((recognition.getWidth()/2) + recognition.getLeft());
+                        if (yPixel <= (recognition.getWidth()/3)) {
+                            label = 1;
+                        } else if (yPixel <= (recognition.getWidth()/1.5)) {
+                            label = 2;
+                        } else {
+                            label = 3;
+                        }
                     }
                 }
                 confidence = -1;
-                return label;
+                return 0;
             }
         }
         confidence = -1;
-        return null;
+        return 0;
+    }
+
+    public void imageReturnTel() {
+        List<Recognition> recognitionsList = tfod.getRecognitions(); // Creates a list with every image detected
+        if (recognitionsList != null) { // This will run if at least one image has been detected
+            telemetry.addData("Number Of Images Detected", recognitionsList.size());
+
+            for (Recognition recognition : recognitionsList) {
+                // This "for" statement will run for every image detected
+                double col = (recognition.getLeft() + recognition.getRight()) / 2; // Finds the "X" position of the image on the webcam stream
+                double row = (recognition.getTop() + recognition.getBottom()) / 2; // Finds the "Y" position of the image on the webcam stream
+                double width = Math.abs(recognition.getRight() - recognition.getLeft()); // Finds the width of the image on the webcam stream
+                double height = Math.abs(recognition.getTop() - recognition.getBottom()); // Finds the heights of the image on the webcam stream
+
+                // Adds a blank line between every image detected
+                telemetry.addData("", " ");
+                // Displays to the phone screen the name of the image and how confident that it is correct
+                telemetry.addData("Image", "%s (%.0f %% Conf.)", recognition.getLabel(), recognition.getConfidence() * 100);
+                telemetry.addData("Pos", (recognition.getWidth()/2) + recognition.getLeft());
+                // Displays to the phone screen the X and Y coordinates of the image. The coordinates are the image's position on the webcam stream
+                telemetry.addData("- Position (Row/Col)", "%.0f / %.0f", row, col);
+                // Displays to the phone screen the size of the image. The size of the image output is the size on the webcam stream
+                telemetry.addData("- Size (Width/Height)", "%.0f / %.0f", width, height);
+            }
+        }
+
     }
 
     public AprilTagDetection aprilTagSearch(int id) {
@@ -141,5 +177,10 @@ public class imageDetection {
         telemetry.addLine("RBE = Range, Bearing & Elevation");
          */
         return null;
+    }
+
+    public void stop() {
+        tfod.shutdown();
+        visionPortal.close();
     }
 }
